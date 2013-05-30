292c8a14e4ae0bd96306cf227ef1ed1b9575c879
Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
2013-05-30 14:34:20 +0530

    sched/fair: Remove unused variable from expire_cfs_rq_runtime()
    Commit 78becc2709 ("sched: Use an accessor to read the rq clock")
    introduces rq_clock(), which obsoletes the use of the "rq" variable
    in expire_cfs_rq_runtime() and triggers this build warning:
    
      kernel/sched/fair.c: In function 'expire_cfs_rq_runtime':
      kernel/sched/fair.c:2159:13: warning: unused variable 'rq' [-Wunused-variable]
    
    Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
    Acked-by: Frederic Weisbecker <fweisbec@gmail.com>
    Acked-by: Paul Turner <pjt@google.com>
    Cc: peterz@infradead.org
    Link: http://lkml.kernel.org/r/1369904660-14169-1-git-send-email-kamalesh@linux.vnet.ibm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2369,7 +2369,6 @@ static int assign_cfs_rq_runtime(struct cfs_rq *cfs_rq)
 static void expire_cfs_rq_runtime(struct cfs_rq *cfs_rq)
 {
 	struct cfs_bandwidth *cfs_b = tg_cfs_bandwidth(cfs_rq->tg);
-	struct rq *rq = rq_of(cfs_rq);
 
 	/* if the deadline is ahead of our clock, nothing to do */
 	if (likely((s64)(rq->clock - cfs_rq->runtime_expires) < 0))
