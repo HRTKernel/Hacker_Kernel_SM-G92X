a81d120aa4fdeaf2802b5d83baec703f029b6b80
HRTKernel <support@hacker-kernel.com>
2015-11-14 11:24:26 +0100

    ramdisk: add aroma installer

--- build_image/boot/ramdisk/sbin/hacker.sh
+++ build_image/boot/ramdisk/sbin/hacker.sh
@@ -1,7 +1,33 @@
 #!/system/bin/sh
 # thanks to mythos234 for the script
 
-/sbin/busybox mount -t rootfs -o remount,rw rootfs
+# VARS
+
+BB=/system/xbin/busybox
+PROP=/system/kernel.prop
+GOVLITTLE=/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+GOVBIG=/sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+FREQMINLITTLE1=/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+FREQMINLITTLE2=/sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+FREQMINLITTLE3=/sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+FREQMINLITTLE4=/sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+FREQMAXLITTLE1=/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+FREQMAXLITTLE2=/sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+FREQMAXLITTLE3=/sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+FREQMAXLITTLE4=/sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+FREQMINBIG1=/sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+FREQMINBIG2=/sys/devices/system/cpu/cpu5/cpufreq/scaling_min_freq
+FREQMINBIG3=/sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq
+FREQMINBIG4=/sys/devices/system/cpu/cpu7/cpufreq/scaling_min_freq
+FREQMAXBIG1=/sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+FREQMAXBIG2=/sys/devices/system/cpu/cpu5/cpufreq/scaling_max_freq
+FREQMAXBIG3=/sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq
+FREQMAXBIG4=/sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq
+LOGS=/data/media/0/hackerkernel/Logs;
+VALUES=/data/media/0/hackerkernel/values;
+
+mount -o remount,rw /system
+mount -o remount,rw /data
 
 sync
 
@@ -28,54 +54,17 @@ fi
 #Set default values on boot
 echo "temporary none" > /sys/class/scsi_disk/0:0:0:1/cache_type
 echo "temporary none" > /sys/class/scsi_disk/0:0:0:2/cache_type
-echo "Set default values on boot successful." >> /data/hackertest.log
-
-#Symlink busybox parts
-#cd /sbin
-#for i in $(./busybox --list)
-#do
-#	./busybox ln -s busybox $i
-#done
-#echo "Busybox symlinks created successful." >> /data/hackertest.log
-#cd /
+echo "Set deepsleep values on boot successful." >> /data/hackertest.log
 
 echo  Kernel script is working !!! >> /data/hackertest.log
 echo "excecuted on $(date +"%d-%m-%Y %r" )" >> /data/hackertest.log
 echo  Done ! >> /data/hackertest.log
 
-# Init.d
-busybox run-parts /system/etc/init.d
-/sbin/busybox mount -t rootfs -o remount,ro rootfs
-/sbin/busybox mount -o remount,rw /data
 
 #Synapse profile
 if [ ! -f /data/.hackerkernel/bck_prof ]; then
 	cp -f /res/synapse/files/bck_prof /data/.hackerkernel/bck_prof
 fi
-
-
-# frandom permissions
-# chmod 444 /dev/erandom
-# chmod 444 /dev/frandom
-
-# turn frandom on by default.
-#if [ ! -f /data/.hackerkernel/setting_frandom ]; then
-#	echo 1 > /data/.hackerkernel/setting_frandom
-#fi;
-
-#setting_frandom=`cat /data/.hackerkernelsetting_frandom`
-
-#if [[ $setting_frandom == 1 ]]; then
-#	insmod /system/lib/modules/frandom.ko
-#	chmod 644 /dev/frandom
-#	chmod 644 /dev/erandom
-#	mv /dev/random /dev/random.ori
-#	mv /dev/urandom /dev/urandom.ori
-#	ln /dev/frandom /dev/random
-#	chmod 644 /dev/random
-#	ln /dev/erandom /dev/urandom
-#	chmod 644 /dev/urandom
-#fi;
 
 chmod 777 /data/.hackerkernel/bck_prof
 
@@ -87,57 +76,340 @@ echo "0x0C47 0x03F5 0x0EE4 0x1D04 0xF1F7 0x040B 0x07C8 0x187D 0xF3B9 0x040A 0x0E
 echo "Set default sound values on boot successful." >> /data/hackertest.log
 
 #Setup Mhz Min/Max Cluster 0
-echo 200000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
 echo 1500000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
-echo 200000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq;
 echo 1500000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq;
-echo 200000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq;
 echo 1500000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq;
-echo 200000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq;
 echo 1500000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq;
 
 #Setup Mhz Min/Max Cluster 1
-echo 200000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq;
 echo 2100000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq;
-echo 200000 > /sys/devices/system/cpu/cpu5/cpufreq/scaling_min_freq;
 echo 2100000 > /sys/devices/system/cpu/cpu5/cpufreq/scaling_max_freq;
-echo 200000 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq;
 echo 2100000 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq;
-echo 200000 > /sys/devices/system/cpu/cpu7/cpufreq/scaling_min_freq;
 echo 2100000 > /sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq;
 
 echo "Set default Min/Max Clusters values on boot successful." >> /data/hackertest.log
 
-# Google Services battery drain fixer by Alcolawl@xda
-# http://forum.xda-developers.com/google-nexus-5/general/script-google-play-services-battery-t3059585/post59563859
-su -c pm enable com.google.android.gms/.update.SystemUpdateActivity 
-su -c pm enable com.google.android.gms/.update.SystemUpdateService
-su -c pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver 
-su -c pm enable com.google.android.gms/.update.SystemUpdateService$Receiver 
-su -c pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver 
-su -c pm enable com.google.android.gsf/.update.SystemUpdateActivity 
-su -c pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity 
-su -c pm enable com.google.android.gsf/.update.SystemUpdateService 
-su -c pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver 
-su -c pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
-
-echo "GApps fix applied successful." >> /data/hackertest.log
-
-LOGS=/data/media/0/hackerkernel/Logs;
-
 if [ ! -d $LOGS ]; then
-	mkdir /data/media/0/hackerkernel/Logs
+	$BB mkdir /data/media/0/hackerkernel/Logs
 fi;
 
-chmod 777 $LOGS
-
-VALUES=/data/media/0/hackerkernel/values;
+$BB chmod -R 0777 $LOGS
 
 if [ ! -d $VALUES ]; then
-	mkdir /data/media/0/hackerkernel/values
+	$BB mkdir /data/media/0/hackerkernel/values
 fi;
-chmod 777 $VALUES
+$BB chmod -R 0777 $VALUES
 
 echo "Interactive" > /data/media/0/hackerkernel/values/gpu_gov
 
-chmod 777 /data/media/0/hackerkernel/values/gpu_gov
+$BB chmod -R 0777 /data/media/0/hackerkernel/values/gpu_gov
+
+echo "Set default folder successful." >> /data/hackertest.log
+
+$BB chown -R system:system /data/anr
+$BB chown -R root:root /tmp
+$BB chown -R root:root /res
+$BB chown -R root:root /sbin
+$BB chown -R root:root /lib
+$BB chmod -R 777 /tmp/
+$BB chmod -R 775 /res/
+$BB chmod -R 0777 /data/anr/
+$BB chmod -R 0400 /data/tombstones
+$BB chown -R root:root /data/property
+$BB chmod -R 0700 /data/property
+$BB chmod 06755 /sbin/busybox
+echo "- Critical Permissions fixed successful." >> /data/hackertest.log
+
+# Parse init/d support from prop. If AUTO mode check to see if support has been added to the rom
+if [ "`grep "kernel.initd=true" $PROP`" != "" ]; then
+	if [ "`grep "init.d" /system/etc/init.sec.boot.sh`" = "" ]; then
+		mount -t rootfs -o remount,rw rootfs
+	fi
+fi
+
+# Parse Mode Enforcement from prop
+if [ "`grep "kernel.turbo=true" $PROP`" != "" ]; then
+	echo "1" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/enforced_mode
+	echo "1" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/enforced_mode
+fi
+
+if [ "`grep "kernel.turbo=false" $PROP`" != "" ]; then
+	echo "0" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/enforced_mode
+	echo "0" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/enforced_mode
+fi
+
+# Wait for 5 second so we pass out of init before starting the rest of the script
+sleep 5
+
+# Parse IO Scheduler from prop
+if [ "`grep "kernel.scheduler=noop" $PROP`" != "" ]; then
+	echo "noop" > /sys/block/mmcblk0/queue/scheduler
+    	echo "noop" > /sys/block/sda/queue/scheduler
+    	echo "noop" > /sys/block/sdb/queue/scheduler
+    	echo "noop" > /sys/block/sdc/queue/scheduler
+    	echo "noop" > /sys/block/vnswap0/queue/scheduler
+elif [ "`grep "kernel.scheduler=sioplus" $PROP`" != "" ]; then
+	echo "sioplus" > /sys/block/mmcblk0/queue/scheduler
+    	echo "sioplus" > /sys/block/sda/queue/scheduler
+    	echo "sioplus" > /sys/block/sdb/queue/scheduler
+    	echo "sioplus" > /sys/block/sdc/queue/scheduler
+    	echo "sioplus" > /sys/block/vnswap0/queue/scheduler
+elif [ "`grep "kernel.scheduler=bfq" $PROP`" != "" ]; then
+	echo "bfq" > /sys/block/mmcblk0/queue/scheduler
+    	echo "bfq" > /sys/block/sda/queue/scheduler
+    	echo "bfq" > /sys/block/sdb/queue/scheduler
+    	echo "bfq" > /sys/block/sdc/queue/scheduler
+    	echo "bfq" > /sys/block/vnswap0/queue/scheduler
+elif [ "`grep "kernel.scheduler=tripndroid" $PROP`" != "" ]; then
+	echo "tripndroid" > /sys/block/mmcblk0/queue/scheduler
+    	echo "tripndroid" > /sys/block/sda/queue/scheduler
+    	echo "tripndroid" > /sys/block/sdb/queue/scheduler
+    	echo "tripndroid" > /sys/block/sdc/queue/scheduler
+    	echo "tripndroid" > /sys/block/vnswap0/queue/scheduler
+elif [ "`grep "kernel.scheduler=cfg" $PROP`" != "" ]; then
+	echo "cfq" > /sys/block/mmcblk0/queue/scheduler
+    	echo "cfq" > /sys/block/sda/queue/scheduler
+    	echo "cfq" > /sys/block/sdb/queue/scheduler
+    	echo "cfq" > /sys/block/sdc/queue/scheduler
+    	echo "cfq" > /sys/block/vnswap0/queue/scheduler
+else
+	echo "cfq" > /sys/block/mmcblk0/queue/scheduler
+    	echo "cfq" > /sys/block/sda/queue/scheduler
+    	echo "cfq" > /sys/block/sdb/queue/scheduler
+    	echo "cfq" > /sys/block/sdc/queue/scheduler
+    	echo "cfq" > /sys/block/vnswap0/queue/scheduler
+fi
+
+# Parse Governor from prop
+if [ "`grep "kernel.governor=alucard" $PROP`" != "" ]; then
+	echo "alucard" > $GOVLITTLE
+	echo "alucard" > $GOVBIG
+elif [ "`grep "kernel.governor=bioshock" $PROP`" != "" ]; then
+	echo "bioshock" > $GOVLITTLE
+	echo "bioshock" > $GOVBIG
+elif [ "`grep "kernel.governor=conservative" $PROP`" != "" ]; then
+	echo "conservative" > $GOVLITTLE
+	echo "conservative" > $GOVBIG
+elif [ "`grep "kernel.governor=conservativex" $PROP`" != "" ]; then
+	echo "conservativex" > $GOVLITTLE
+	echo "conservativex" > $GOVBIG
+elif [ "`grep "kernel.governor=dancedance" $PROP`" != "" ]; then
+	echo "dancedance" > $GOVLITTLE
+	echo "dancedance" > $GOVBIG
+elif [ "`grep "kernel.governor=darkness" $PROP`" != "" ]; then
+	echo "darkness" > $GOVLITTLE
+	echo "darkness" > $GOVBIG
+elif [ "`grep "kernel.governor=hyper" $PROP`" != "" ]; then
+	echo "hyper" > $GOVLITTLE
+	echo "hyper" > $GOVBIG
+elif [ "`grep "kernel.governor=interactive" $PROP`" != "" ]; then
+	echo "interactive" > $GOVLITTLE
+	echo "interactive" > $GOVBIG
+elif [ "`grep "kernel.governor=lionheart" $PROP`" != "" ]; then
+	echo "Lionheart" > $GOVLITTLE
+	echo "Lionheart" > $GOVBIG
+elif [ "`grep "kernel.governor=nightmare" $PROP`" != "" ]; then
+	echo "nightmare" > $GOVLITTLE
+	echo "nightmare" > $GOVBIG
+elif [ "`grep "kernel.governor=ondemand" $PROP`" != "" ]; then
+	echo "ondemand" > $GOVLITTLE
+	echo "ondemand" > $GOVBIG
+elif [ "`grep "kernel.governor=ondemandplus" $PROP`" != "" ]; then
+	echo "ondemandplus" > $GOVLITTLE
+	echo "ondemandplus" > $GOVBIG
+elif [ "`grep "kernel.governor=performance" $PROP`" != "" ]; then
+	echo "performance" > $GOVLITTLE
+	echo "performance" > $GOVBIG
+elif [ "`grep "kernel.governor=preservative" $PROP`" != "" ]; then
+	echo "preservative" > $GOVLITTLE
+	echo "preservative" > $GOVBIG
+elif [ "`grep "kernel.governor=smartass2" $PROP`" != "" ]; then
+	echo "smartassV2" > $GOVLITTLE
+	echo "smartassV2" > $GOVBIG
+elif [ "`grep "kernel.governor=userspace" $PROP`" != "" ]; then
+	echo "userspace" > $GOVLITTLE
+	echo "userspace" > $GOVBIG
+elif [ "`grep "kernel.governor=wheatley" $PROP`" != "" ]; then
+	echo "wheatley" > $GOVLITTLE
+	echo "wheatley" > $GOVBIG
+else 
+	echo "interactive" > $GOVLITTLE
+	echo "interactive" > $GOVBIG
+fi
+
+sleep 1;
+
+# Parse CPU CLOCK from prop
+if [ "`grep "kernel.cpu.a53.min=200000" $PROP`" != "" ]; then
+	echo "200000" > $FREQMINLITTLE1
+	echo "200000" > $FREQMINLITTLE2
+	echo "200000" > $FREQMINLITTLE3
+	echo "200000" > $FREQMINLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=300000" $PROP`" != "" ]; then
+	echo "300000" > $FREQMINLITTLE1
+	echo "300000" > $FREQMINLITTLE2
+	echo "300000" > $FREQMINLITTLE3
+	echo "300000" > $FREQMINLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=400000" $PROP`" != "" ]; then
+	echo "400000" > $FREQMINLITTLE1
+	echo "400000" > $FREQMINLITTLE2
+	echo "400000" > $FREQMINLITTLE3
+	echo "400000" > $FREQMINLITTLE4
+else
+	echo "400000" > $FREQMINLITTLE1
+	echo "400000" > $FREQMINLITTLE2
+	echo "400000" > $FREQMINLITTLE3
+	echo "400000" > $FREQMINLITTLE4
+fi
+
+sleep 1;
+
+if [ "`grep "kernel.cpu.a53.max=1200000" $PROP`" != "" ]; then
+	echo "1200000" > $FREQMAXLITTLE1
+	echo "1200000" > $FREQMAXLITTLE2
+	echo "1200000" > $FREQMAXLITTLE3
+	echo "1200000" > $FREQMAXLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=1296000" $PROP`" != "" ]; then
+	echo "1296000" > $FREQMAXLITTLE1
+	echo "1296000" > $FREQMAXLITTLE2
+	echo "1296000" > $FREQMAXLITTLE3
+	echo "1296000" > $FREQMAXLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=1400000" $PROP`" != "" ]; then
+	echo "1400000" > $FREQMAXLITTLE1
+	echo "1400000" > $FREQMAXLITTLE2
+	echo "1400000" > $FREQMAXLITTLE3
+	echo "1400000" > $FREQMAXLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=1500000" $PROP`" != "" ]; then
+	echo "1500000" > $FREQMAXLITTLE1
+	echo "1500000" > $FREQMAXLITTLE2
+	echo "1500000" > $FREQMAXLITTLE3
+	echo "1500000" > $FREQMAXLITTLE4
+elif [ "`grep "kernel.cpu.a53.min=1600000" $PROP`" != "" ]; then
+	echo "1600000" > $FREQMAXLITTLE1
+	echo "1600000" > $FREQMAXLITTLE2
+	echo "1600000" > $FREQMAXLITTLE3
+	echo "1600000" > $FREQMAXLITTLE4
+else
+	echo "1500000" > $FREQMAXLITTLE1
+	echo "1500000" > $FREQMAXLITTLE2
+	echo "1500000" > $FREQMAXLITTLE3
+	echo "1500000" > $FREQMAXLITTLE4
+fi
+
+sleep 1;
+
+if [ "`grep "kernel.cpu.a57.min=200000" $PROP`" != "" ]; then
+	echo "200000" > $FREQMINBIG1
+	echo "200000" > $FREQMINBIG2
+	echo "200000" > $FREQMINBIG3
+	echo "200000" > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=300000" $PROP`" != "" ]; then
+	echo "300000" > $FREQMINBIG1
+	echo "300000" > $FREQMINBIG2
+	echo "300000" > $FREQMINBIG3
+	echo "300000" > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=400000" $PROP`" != "" ]; then
+	echo "400000" > $FREQMINBIG1
+	echo "400000" > $FREQMINBIG2
+	echo "400000" > $FREQMINBIG3
+	echo "400000" > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=500000" $PROP`" != "" ]; then
+	echo "500000" > $FREQMINBIG1
+	echo "500000" > $FREQMINBIG2
+	echo "500000" > $FREQMINBIG3
+	echo "500000" > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=600000" $PROP`" != "" ]; then
+	echo "600000" > $FREQMINBIG1
+	echo "600000" > $FREQMINBIG2
+	echo "600000" > $FREQMINBIG3
+	echo "600000" > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=700000" $PROP`" != "" ]; then
+	echo "700000" > $FREQMINBIG1
+	echo "700000" > $FREQMINBIG2
+	echo "700000" > $FREQMINBIG3
+	echo "700000" > $FREQMINBIG4
+elif [ "`grep "kernel.cpu.a57.min=800000" $PROP`" != "" ]; then
+	echo "800000" > $FREQMINBIG1
+	echo "800000" > $FREQMINBIG2
+	echo "800000" > $FREQMINBIG3
+	echo "800000" > $FREQMINBIG4
+else
+	echo "800000" > $FREQMINBIG1
+	echo "800000" > $FREQMINBIG2
+	echo "800000" > $FREQMINBIG3
+	echo "800000" > $FREQMINBIG4
+fi
+
+sleep 1;
+
+if [ "`grep "kernel.cpu.a57.max=1704000" $PROP`" != "" ]; then
+	echo "1704000" > $FREQMAXBIG1
+	echo "1704000" > $FREQMAXBIG2
+	echo "1704000" > $FREQMAXBIG3
+	echo "1704000" > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=1800000" $PROP`" != "" ]; then
+	echo "1800000" > $FREQMAXBIG1
+	echo "1800000" > $FREQMAXBIG2
+	echo "1800000" > $FREQMAXBIG3
+	echo "1800000" > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=1896000" $PROP`" != "" ]; then
+	echo "1896000" > $FREQMAXBIG1
+	echo "1896000" > $FREQMAXBIG2
+	echo "1896000" > $FREQMAXBIG3
+	echo "1896000" > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=2000000" $PROP`" != "" ]; then
+	echo "2000000" > $FREQMAXBIG1
+	echo "2000000" > $FREQMAXBIG2
+	echo "2000000" > $FREQMAXBIG3
+	echo "2000000" > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=2100000" $PROP`" != "" ]; then
+	echo "2100000" > $FREQMAXBIG1
+	echo "2100000" > $FREQMAXBIG2
+	echo "2100000" > $FREQMAXBIG3
+	echo "2100000" > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=2200000" $PROP`" != "" ]; then
+	echo "2200000" > $FREQMAXBIG1
+	echo "2200000" > $FREQMAXBIG2
+	echo "2200000" > $FREQMAXBIG3
+	echo "2200000" > $FREQMAXBIG4
+elif [ "`grep "kernel.cpu.a57.max=2304000" $PROP`" != "" ]; then
+	echo "2304000" > $FREQMAXBIG1
+	echo "2304000" > $FREQMAXBIG2
+	echo "2304000" > $FREQMAXBIG3
+	echo "2304000" > $FREQMAXBIG4
+else
+	echo "2100000" > $FREQMAXBIG1
+	echo "2100000" > $FREQMAXBIG2
+	echo "2100000" > $FREQMAXBIG3
+	echo "2100000" > $FREQMAXBIG4
+fi
+
+sleep 1;
+
+# Parse GApps wakelock fix from prop
+if [ "`grep "kernel.gapps=true" $PROP`" != "" ]; then
+	sleep 60
+	su -c "pm enable com.google.android.gms/.update.SystemUpdateActivity"
+	su -c "pm enable com.google.android.gms/.update.SystemUpdateService"
+	su -c "pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver"
+	su -c "pm enable com.google.android.gms/.update.SystemUpdateService$Receiver"
+	su -c "pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver"
+	su -c "pm enable com.google.android.gsf/.update.SystemUpdateActivity"
+	su -c "pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity"
+	su -c "pm enable com.google.android.gsf/.update.SystemUpdateService"
+	su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver"
+	su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver"
+fi
+
+# Execute init.d if Auto or ROM control
+if [ "`grep "kernel.initd=true" $PROP`" != "" ]; then
+	#enforce init.d script perms on any post-root added files
+	chmod 755 /system/etc/init.d
+	chmod 755 /system/etc/init.d/*
+	if [ "`grep "init.d" /system/etc/init.sec.boot.sh`" = "" ]; then
+		# run init.d scripts
+		if [ -d /system/etc/init.d ]; then
+		  run-parts /system/etc/init.d
+		fi
+	fi
+fi
---
+++ build_image/zip_files/kernel/aroma.sh
@@ -0,0 +1,234 @@
+#!/sbin/sh
+# Base by glewarne
+# Modded for more tweaking by thehacker911
+
+# VARS
+
+PROP=/system/kernel.prop
+
+if [ "$1" = "WAIT" ]; then	
+	sleep 0.7
+elif [ "$1" = "FLASH" ]; then	
+	dd if=/tmp/boot.img of=/dev/block/platform/15570000.ufs/by-name/BOOT
+elif [ "$1" = "FIX_PERMS" ]; then	
+	chmod 644 /system/build.prop
+	chmod 644 $PROP
+elif [ "$1" = "DETECT" ]; then	
+	# Probe /proc/cmdline to detect variant
+	if [ "`grep "G920T" /proc/cmdline`" != "" ]; then
+		# found G920 T variant	
+		D=T
+	elif [ "`grep "G920W8" /proc/cmdline`" != "" ]; then
+		# found G920 W8 variant	
+		D=T
+	elif [ "`grep "G925T" /proc/cmdline`" != "" ]; then
+		# found G925 T variant	
+		D=T
+	elif [ "`grep "G925W8" /proc/cmdline`" != "" ]; then
+		# found G925 W8 variant	
+		D=T
+	elif [ "`grep "G920P" /proc/cmdline`" != "" ]; then
+		# found G920 P variant
+		D=P
+	elif [ "`grep "G925P" /proc/cmdline`" != "" ]; then
+		# found G925 P variant
+		D=P
+	elif [ "`grep "G920R" /proc/cmdline`" != "" ]; then
+		# found G925 R variant
+		D=P
+	elif [ "`grep "G925R" /proc/cmdline`" != "" ]; then
+		# found G925 R variant
+		D=P
+	else
+		# must be an international variant, assume F
+		D=F
+	fi
+	if [ "$D" = "P" ]; then
+		echo "device.type=incompatible" > /system/device.prop
+	else
+		echo "device.type=compatible" > /system/device.prop
+	fi
+	if [ "$D" = "F" ]; then
+		if [ "`grep "G925" /proc/cmdline`" != "" ]; then
+			echo "device.model=intedgeF" >> /system/device.prop
+		else
+			echo "device.model=intflatF" >> /system/device.prop
+		fi
+	else
+		if [ "`grep "G925" /proc/cmdline`" != "" ]; then
+			echo "device.model=intedgeT" >> /system/device.prop
+		else
+			echo "device.model=intflatT" >> /system/device.prop
+		fi
+	fi
+	sleep 0.7
+
+# SCHEDULER
+elif [ "$1" = "CFQ" ]; then
+	echo "kernel.scheduler=cfq" >> $PROP
+
+elif [ "$1" = "BFQ" ]; then
+	echo "kernel.scheduler=bfq" >> $PROP
+
+elif [ "$1" = "NOOP" ]; then
+	echo "kernel.scheduler=noop" >> $PROP
+
+elif [ "$1" = "SIOPLUS" ]; then
+	echo "kernel.scheduler=sioplus" >> $PROP
+
+elif [ "$1" = "TRIPNDROID" ]; then
+	echo "kernel.scheduler=tripndroid" >> $PROP
+
+# GOVS
+
+elif [ "$1" = "ALUCARD" ]; then
+	echo "kernel.governor=alucard" >> $PROP
+
+elif [ "$1" = "BIOSHOCK" ]; then
+	echo "kernel.governor=bioshock" >> $PROP
+
+elif [ "$1" = "CONSERVATIVE" ]; then
+	echo "kernel.governor=conservative" >> $PROP
+
+elif [ "$1" = "CONSERVATIVEX" ]; then
+	echo "kernel.governor=conservativex" >> $PROP
+
+elif [ "$1" = "DANCEDANCE" ]; then
+	echo "kernel.governor=dancedance" >> $PROP
+
+elif [ "$1" = "DARKNESS" ]; then
+	echo "kernel.governor=darkness" >> $PROP
+
+elif [ "$1" = "HYPER" ]; then
+	echo "kernel.governor=hyper" >> $PROP
+
+elif [ "$1" = "INTERACTIVE" ]; then
+	echo "kernel.governor=interactive" >> $PROP
+
+elif [ "$1" = "LIONHEART" ]; then
+	echo "kernel.governor=lionheart" >> $PROP
+
+elif [ "$1" = "NIGHTMARE" ]; then
+	echo "kernel.governor=nightmare" >> $PROP
+
+elif [ "$1" = "ONDEMAND" ]; then
+	echo "kernel.governor=ondemand" >> $PROP
+
+elif [ "$1" = "ONDEMANDPLUS" ]; then
+	echo "kernel.governor=ondemandplus" >> $PROP
+
+elif [ "$1" = "PERFORMANCE" ]; then
+	echo "kernel.governor=performance" >> $PROP
+
+elif [ "$1" = "PRESERVATIVE" ]; then
+	echo "kernel.governor=preservative" >> $PROP
+
+elif [ "$1" = "SMARTASS2" ]; then
+	echo "kernel.governor=smartass2" >> $PROP
+
+elif [ "$1" = "USERSPACE" ]; then
+	echo "kernel.governor=userspace" >> $PROP
+
+elif [ "$1" = "WHEATLEY" ]; then
+	echo "kernel.governor=wheatley" >> $PROP
+sleep 0.7
+
+# CPU CLOCK A53
+
+elif [ "$1" = "A53-200" ]; then
+	echo "kernel.cpu.a53.min=200000" >> $PROP
+
+elif [ "$1" = "A53-300" ]; then
+	echo "kernel.cpu.a53.min=300000" >> $PROP
+
+elif [ "$1" = "A53-400" ]; then
+	echo "kernel.cpu.a53.min=400000" >> $PROP
+
+elif [ "$1" = "A53-1200" ]; then
+	echo "kernel.cpu.a53.max=1200000" >> $PROP
+
+elif [ "$1" = "A53-1296" ]; then
+	echo "kernel.cpu.a53.max=1296000" >> $PROP
+
+elif [ "$1" = "A53-1400" ]; then
+	echo "kernel.cpu.a53.max=1400000" >> $PROP
+
+elif [ "$1" = "A53-1500" ]; then
+	echo "kernel.cpu.a53.max=1500000" >> $PROP
+
+elif [ "$1" = "A53-1600" ]; then
+	echo "kernel.cpu.a53.max=1600000" >> $PROP
+sleep 0.7
+
+# CPU CLOCK A57
+
+elif [ "$1" = "A57-200" ]; then
+	echo "kernel.cpu.a57.min=200000" >> $PROP
+
+elif [ "$1" = "A57-300" ]; then
+	echo "kernel.cpu.a57.min=300000" >> $PROP
+
+elif [ "$1" = "A57-400" ]; then
+	echo "kernel.cpu.a57.min=400000" >> $PROP
+
+elif [ "$1" = "A57-500" ]; then
+	echo "kernel.cpu.a57.min=500000" >> $PROP
+
+elif [ "$1" = "A57-600" ]; then
+	echo "kernel.cpu.a57.min=600000" >> $PROP
+
+elif [ "$1" = "A57-700" ]; then
+	echo "kernel.cpu.a57.min=700000" >> $PROP
+
+elif [ "$1" = "A57-800" ]; then
+	echo "kernel.cpu.a57.min=800000" >> $PROP
+
+elif [ "$1" = "A57-1704" ]; then
+	echo "kernel.cpu.a57.max=1704000" >> $PROP
+
+elif [ "$1" = "A57-1800" ]; then
+	echo "kernel.cpu.a57.max=1800000" >> $PROP
+
+elif [ "$1" = "A57-1896" ]; then
+	echo "kernel.cpu.a57.max=1896000" >> $PROP
+
+elif [ "$1" = "2000" ]; then
+	echo "kernel.cpu.a57.max=2000000" >> $PROP
+
+elif [ "$1" = "A57-2100" ]; then
+	echo "kernel.cpu.a57.max=2100000" >> $PROP
+
+elif [ "$1" = "A57-2200" ]; then
+	echo "kernel.cpu.a57.max=2200000" >> $PROP
+
+elif [ "$1" = "A57-2304" ]; then
+	echo "kernel.cpu.a57.max=2304000" >> $PROP
+sleep 0.7
+
+elif [ "$1" = "TURBO" ]; then
+	echo "kernel.turbo=true" >> $PROP
+
+elif [ "$1" = "NO_TURBO" ]; then
+	echo "kernel.turbo=false" >> $PROP
+
+elif [ "$1" = "BOOST" ]; then
+	echo "kernel.inputboost=true" >> $PROP
+
+elif [ "$1" = "NO_BOOST" ]; then
+	echo "kernel.inputboost=false" >> $PROP
+
+elif [ "$1" = "AUTO_INITD" ]; then
+	echo "kernel.initd=true" >> $PROP
+
+elif [ "$1" = "NO_INITD" ]; then
+	echo "kernel.initd=false" >> $PROP
+
+elif [ "$1" = "FIX_GAPPS" ]; then
+	echo "kernel.gapps=true" >> $PROP
+
+elif [ "$1" = "STOCK_GAPPS" ]; then
+	echo "kernel.gapps=false" >> $PROP
+
+
+fi
+
