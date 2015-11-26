581cf4ba4f6043c5e934ada94c9bce44ecc8bb0b
Park Ju Hyung <qkrwngud825@gmail.com>
2015-11-26 21:39:40 +0900

    dm_cpu_hotplug: poll every 1000ms if screen is on
    Hotplugging is rarely used while the screen is on.
    Polling 10 times every seconds is stupid.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/cpufreq/dm_cpu_hotplug.c
+++ drivers/cpufreq/dm_cpu_hotplug.c
@@ -42,7 +42,8 @@
 #else
 #define NORMALMIN_FREQ	500000
 #endif
-#define POLLING_MSEC	100
+#define POLLING_MSEC_DISP_ON	1000
+#define POLLING_MSEC_DISP_OFF	100
 #define DEFAULT_LOW_STAY_THRSHD	0
 
 #define MIN_NUM_ONLINE_CPU	1
@@ -117,7 +118,7 @@ static void calc_load(void);
 
 static enum hotplug_cmd prev_cmd = CMD_NORMAL;
 static enum hotplug_cmd exe_cmd;
-static unsigned int delay = POLLING_MSEC;
+static unsigned int delay = POLLING_MSEC_DISP_ON;
 
 #if defined(CONFIG_SCHED_HMP)
 static struct workqueue_struct *hotplug_wq;
@@ -551,6 +552,8 @@ static int fb_state_change(struct notifier_block *nb,
 		pr_info("LCD is off\n");
 		screen_is_on = false;
 
+		delay = POLLING_MSEC_DISP_OFF;
+
 #ifdef CONFIG_HOTPLUG_THREAD_STOP
 		if (thread_manage_wq) {
 			if (work_pending(&manage_work))
@@ -569,6 +572,8 @@ static int fb_state_change(struct notifier_block *nb,
 		lcd_is_on = true;
 		screen_is_on = true;
 		pr_info("LCD is on\n");
+
+		delay = POLLING_MSEC_DISP_ON;
 
 #ifdef CONFIG_HOTPLUG_THREAD_STOP
 		if (thread_manage_wq) {
