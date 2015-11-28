80c2d87a8d74dbfbf4bc2929e62934aac167410b
UpInTheAir <upintheair.xda@gmail.com>
2015-11-28 09:17:49 +0700

    Linux 3.10.93
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -2381,6 +2381,11 @@ again:
 			break;
 		}
 
+		if (fatal_signal_pending(current)) {
+			status = -EINTR;
+			break;
+		}
+
 		status = a_ops->write_begin(file, mapping, pos, bytes, flags,
 						&page, &fsdata);
 		if (unlikely(status))
@@ -2421,10 +2426,6 @@ again:
 		written += copied;
 
 		balance_dirty_pages_ratelimited(mapping);
-		if (fatal_signal_pending(current)) {
-			status = -EINTR;
-			break;
-		}
 	} while (iov_iter_count(i));
 
 	return written ? written : status;
