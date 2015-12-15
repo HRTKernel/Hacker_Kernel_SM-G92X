93c1bb4aaafda677f6f9264635931d4015a39fa9
HRTKernel <support@hacker-kernel.com>
2015-12-15 01:18:50 +0100

    build: update script for kernel with sound mod

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,6 +5,21 @@ KERNEL_DIR=$(pwd)
 BUILD_USER="$USER"
 TOOLCHAIN_DIR=/home/$BUILD_USER/android/toolchains
 BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
+BUILD_WHERE=$(pwd)
+BUILD_KERNEL_DIR=$BUILD_WHERE
+BOARD_KERNEL_PAGESIZE=2048
+BOOTIMG=$BUILD_KERNEL_DIR/build_image/boot.img
+BOOTIMG_DIR=build_image/boot
+BOOTIMG_DIR_2=build_image
+DTBTOOL=$BUILD_KERNEL_DIR/tools/dtbtool
+FLASH_ZIP_FILES=zip_files
+FLASH_ZIP_DIR=$FLASH_ZIP_FILES/$KERNEL_NAME
+KERNEL_ZIMG=$BUILD_KERNEL_DIR/arch/arm64/boot/Image
+OUTPUT_DIR=$BUILD_KERNEL_DIR/build_image/output_kernel
+ZIP_VER=`sed -n '8p' thehacker911`
+DEVICE_VER=`sed -n '4p' thehacker911`
+VER=`sed -n '6p' thehacker911`
+KERNEL_NAME="$ZIP_VER$DEVICE_VER$VER"
 
 # Toolchains
 
@@ -53,6 +68,7 @@ BUILD_KERNEL()
 	export KBUILD_BUILD_HOST=smartlounge.eu
         #export USE_CCACHE=1
         export USE_SEC_FIPS_MODE=true
+        export ENABLE_GRAPHITE=true
 	export CROSS_COMPILE=$BUILD_CROSS_COMPILE
 	make ARCH=arm64 $KERNEL_DEFCONFIG
 	make ARCH=arm64 -j$BUILD_JOB_NUMBER
@@ -65,6 +81,73 @@ BUILD_KERNEL()
 	echo ""
 }
 
+REPACK_KERNEL()
+{	
+	echo ""
+	echo "=============================================="
+	echo "START: REPACK_KERNEL"
+	echo "=============================================="
+	echo ""
+	      echo "$KERNEL_NAME" 
+	if [ -e $BUILD_KERNEL_DIR/arch/arm64/boot/Image ]; then
+	      cp -r $KERNEL_ZIMG $BOOTIMG_DIR_2/Image
+
+	      find . -name "*.ko" -exec cp {} $BUILD_KERNEL_DIR/build_image/zip_files/system/lib/modules/ \;
+
+	      rm $BUILD_KERNEL_DIR/build_image/zip_files/system/lib/modules/placeholder
+
+	      cd build_image
+	      rm output_kernel/*.zip
+
+	      echo "Making boot.img ..."
+	      $DTBTOOL -o dt.img -s $BOARD_KERNEL_PAGESIZE -p ../scripts/dtc/ ../arch/arm64/boot/dts/ | sleep 1
+
+	      cp -r Image boot/zImage
+	      chmod a+r dt.img
+	      cp dt.img boot/dt.img
+
+	      ./mkboot boot boot.img
+	      #./mkboot boot.img boot
+
+	      echo "Making zip ..."
+	      cp $BOOTIMG $FLASH_ZIP_FILES/kernel/boot.img
+	      cd $FLASH_ZIP_FILES
+	      zip -r $KERNEL_NAME.zip META-INF system kernel data
+	      mv $KERNEL_NAME.zip $OUTPUT_DIR
+
+	      echo "Making cleaning ..."
+	      find . -name "*.ko" -exec rm {} \;
+	      cd ..
+	      rm dt.img
+	      rm boot.img
+	      rm Image
+	      rm zip_files/boot.img
+	      rm boot/zImage
+	      rm boot/dt.img
+	      cd ..
+	      rm -rf /arch/arm64/boot/dts/.*.tmp
+	      rm -rf /arch/arm64/boot/dts/.*.cmd
+	      rm -rf /arch/arm64/boot/dts/*.dtb
+
+	      echo "All Done!"
+	
+	      echo ""
+	      echo "================================="
+	      echo "END: REPACK_KERNEL"
+	      echo "================================="
+	      echo ""
+	      
+	else
+	
+	      echo ""
+	      echo "================================="
+	      echo "END: FAIL KERNEL BUILD!"
+	      echo "================================="
+	      echo ""
+	      exit 0;
+	fi;
+	
+}
 
 
 
@@ -75,6 +158,7 @@ rm -rf ./build.log
 	START_TIME=`date +%s`
 	BUILD_DATE=`date +%m-%d-%Y`
 	BUILD_KERNEL
+	REPACK_KERNEL
 
 
 	END_TIME=`date +%s`
