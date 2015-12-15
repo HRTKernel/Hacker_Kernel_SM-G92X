8a7fabcdfd4dbcbee31e6fd8378236bf36f03a54
HRTKernel <support@hacker-kernel.com>
2015-12-15 01:21:49 +0100

    build: add script for build kernel without sound mod

---
+++ build_kernel_without_sound_mod.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+# kernel build script by thehacker911
+
+KERNEL_DIR=$(pwd)
+BUILD_USER="$USER"
+TOOLCHAIN_DIR=/home/$BUILD_USER/android/toolchains
+BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
+BUILD_WHERE=$(pwd)
+BUILD_KERNEL_DIR=$BUILD_WHERE
+BOARD_KERNEL_PAGESIZE=2048
+BOOTIMG=$BUILD_KERNEL_DIR/build_image_without_sound_mod/boot.img
+BOOTIMG_DIR=build_image_without_sound_mod/boot
+BOOTIMG_DIR_2=build_image_without_sound_mod
+DTBTOOL=$BUILD_KERNEL_DIR/tools/dtbtool
+FLASH_ZIP_FILES=zip_files
+FLASH_ZIP_DIR=$FLASH_ZIP_FILES/$KERNEL_NAME
+KERNEL_ZIMG=$BUILD_KERNEL_DIR/arch/arm64/boot/Image
+OUTPUT_DIR=$BUILD_KERNEL_DIR/build_image_without_sound_mod/output_kernel
+ZIP_VER=`sed -n '8p' thehacker911`
+DEVICE_VER=`sed -n '4p' thehacker911`
+VER=`sed -n '10p' thehacker911`
+KERNEL_NAME="$ZIP_VER$DEVICE_VER$VER"
+
+# Toolchains
+
+#Sabermod
+#Linaro
+BUILD_CROSS_COMPILE=$TOOLCHAIN_DIR/UBERTC-aarch64-linux-android-6.0/bin/aarch64-linux-android-
+
+
+#vars
+KERNEL_DEFCONFIG=hacker_defconfig
+BASE_VER=`sed -n '2p' thehacker911`
+DEVICE_VER=`sed -n '4p' thehacker911`
+VER=`sed -n '10p' thehacker911`
+HACKER_VER="$BASE_VER$DEVICE_VER$VER"
+
+
+BUILD_KERNEL()
+{	
+	echo ""
+	echo "=============================================="
+	echo "START: MAKE CLEAN"
+	echo "=============================================="
+	echo ""
+	
+
+	make clean
+	find . -name "*.dtb" -exec rm {} \;
+
+	echo ""
+	echo "=============================================="
+	echo "END: MAKE CLEAN"
+	echo "=============================================="
+	echo ""
+
+	echo ""
+	echo "=============================================="
+	echo "START: BUILD_KERNEL"
+	echo "=============================================="
+	echo ""
+	echo "$HACKER_VER" 
+	
+	export LOCALVERSION=-`echo $HACKER_VER`
+	export ARCH=arm64
+        export SUBARCH=arm64
+	export KBUILD_BUILD_USER=thehacker911
+	export KBUILD_BUILD_HOST=smartlounge.eu
+        #export USE_CCACHE=1
+        export USE_SEC_FIPS_MODE=true
+        export ENABLE_GRAPHITE=true
+	export CROSS_COMPILE=$BUILD_CROSS_COMPILE
+	make ARCH=arm64 $KERNEL_DEFCONFIG
+	make ARCH=arm64 -j$BUILD_JOB_NUMBER
+	
+
+	echo ""
+	echo "================================="
+	echo "END: BUILD_KERNEL"
+	echo "================================="
+	echo ""
+}
+
+REPACK_KERNEL()
+{	
+	echo ""
+	echo "=============================================="
+	echo "START: REPACK_KERNEL"
+	echo "=============================================="
+	echo ""
+	      echo "$KERNEL_NAME" 
+	if [ -e $BUILD_KERNEL_DIR/arch/arm64/boot/Image ]; then
+	      cp -r $KERNEL_ZIMG $BOOTIMG_DIR_2/Image
+
+	      find . -name "*.ko" -exec cp {} $BUILD_KERNEL_DIR/build_image_without_sound_mod/zip_files/system/lib/modules/ \;
+
+	      rm $BUILD_KERNEL_DIR/build_image_without_sound_mod/zip_files/system/lib/modules/placeholder
+
+	      cd build_image_without_sound_mod
+	      rm output_kernel/*.zip
+
+	      echo "Making boot.img ..."
+	      $DTBTOOL -o dt.img -s $BOARD_KERNEL_PAGESIZE -p ../scripts/dtc/ ../arch/arm64/boot/dts/ | sleep 1
+
+	      cp -r Image boot/zImage
+	      chmod a+r dt.img
+	      cp dt.img boot/dt.img
+
+	      ./mkboot boot boot.img
+	      #./mkboot boot.img boot
+
+	      echo "Making zip ..."
+	      cp $BOOTIMG $FLASH_ZIP_FILES/kernel/boot.img
+	      cd $FLASH_ZIP_FILES
+	      zip -r $KERNEL_NAME.zip META-INF system kernel data
+	      mv $KERNEL_NAME.zip $OUTPUT_DIR
+
+	      echo "Making cleaning ..."
+	      find . -name "*.ko" -exec rm {} \;
+	      cd ..
+	      rm dt.img
+	      rm boot.img
+	      rm Image
+	      rm zip_files/boot.img
+	      rm boot/zImage
+	      rm boot/dt.img
+	      cd ..
+	      rm -rf /arch/arm64/boot/dts/.*.tmp
+	      rm -rf /arch/arm64/boot/dts/.*.cmd
+	      rm -rf /arch/arm64/boot/dts/*.dtb
+
+	      echo "All Done!"
+	
+	      echo ""
+	      echo "================================="
+	      echo "END: REPACK_KERNEL"
+	      echo "================================="
+	      echo ""
+	      
+	else
+	
+	      echo ""
+	      echo "================================="
+	      echo "END: FAIL KERNEL BUILD!"
+	      echo "================================="
+	      echo ""
+	      exit 0;
+	fi;
+	
+}
+
+
+
+
+# MAIN FUNCTION
+rm -rf ./build.log
+(
+	START_TIME=`date +%s`
+	BUILD_DATE=`date +%m-%d-%Y`
+	BUILD_KERNEL
+	REPACK_KERNEL
+
+
+	END_TIME=`date +%s`
+	let "ELAPSED_TIME=$END_TIME-$START_TIME"
+	echo "Total compile time is $ELAPSED_TIME seconds"
+) 2>&1	 | tee -a ./build.log
+
+# Credits:
+# Samsung
+# google
+# osm0sis
+# cyanogenmod
+# kylon 
