1f60b98f7e9ee4a36e36a129ddd4e65fdcb1d4fd
HRTKernel <support@hacker-kernel.com>
2016-01-06 22:18:22 +0100

    build: update build scripts for build and repack systemless versions

--- build_kernel.sh
+++ build_kernel.sh
@@ -19,6 +19,7 @@ OUTPUT_DIR=$BUILD_KERNEL_DIR/build_image/output_kernel
 ZIP_VER=`sed -n '8p' thehacker911`
 DEVICE_VER=`sed -n '4p' thehacker911`
 VER=`sed -n '6p' thehacker911`
+SU_VER=`sed -n '14p' thehacker911`
 KERNEL_NAME="$ZIP_VER$DEVICE_VER$VER"
 TOOLCHAIN=`sed -n '12p' thehacker911`
 BUILD_CROSS_COMPILE=$TOOLCHAIN_DIR/$TOOLCHAIN
@@ -120,9 +121,24 @@ REPACK_KERNEL()
 	      rm dt.img
 	      rm boot.img
 	      rm Image
-	      rm zip_files/boot.img
+	      rm zip_files/kernel/boot.img
 	      rm boot/zImage
 	      rm boot/dt.img
+	      
+	      echo "repack normal version + su systemless"
+	      cp backup_image/dt.img boot_su_266/dt.img
+	      cp backup_image/zImage boot_su_266/zImage
+	      ./mkboot boot_su_266 boot.img
+	      cp $BOOTIMG $FLASH_ZIP_FILES/kernel/boot.img
+	      cd $FLASH_ZIP_FILES
+	      zip -r $KERNEL_NAME$SU_VER.zip META-INF system kernel data
+	      mv $KERNEL_NAME$SU_VER.zip $OUTPUT_DIR
+	      cd ..
+	      rm boot.img
+	      rm zip_files/kernel/boot.img
+	      rm boot_su_266/zImage
+	      rm boot_su_266/dt.img
+	      
 	      cd ..
 	      rm -rf /arch/arm64/boot/dts/.*.tmp
 	      rm -rf /arch/arm64/boot/dts/.*.cmd
